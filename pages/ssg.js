b27290436c951e1300dcba6d7a9c449c9c614c22
Eliel Sousa <sousaeliel@gmail.com>
2022-11-08 19:40:38 -0300

    change timeout

--- pages/ssg.js
+++ pages/ssg.js
@@ -2,7 +2,7 @@ import Link from "../src/components/Link"
 import delay from "../src/util/delay"
 
 export async function getStaticProps() {
-    await delay(2)
+    await delay(1)
     return {
         props: {
             mensagem: 'Fui gerada durante o build!',
@@ -13,8 +13,8 @@ export async function getStaticProps() {
 export default function SSGPage({ mensagem }) {
     return (
         <di>
-            <h1>{mensagem}</h1>
             <Link href="/">Ir para a Home</Link>
+            <h1>{mensagem}</h1>
         </di>
     )
 }
